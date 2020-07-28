b156cca49d146b3d4197d325f9cd138ea89b1223
taoj <jiatao1993@outlook.com>
2020-07-29 00:50:33 +0800

    wip

--- src/api.py
+++ src/api.py
@@ -81,16 +81,12 @@ class WechatSpider:
     def crawl_latest_posts(self, num, begin=0, count=5):
         while begin < num:
             page_info = []
-            time.sleep(1)
+            time.sleep(0.2)
             resp = self._get_article_list(begin, count)
-            if resp['base_resp']['err_msg'] == 'ok' and resp['base_resp']['ret'] == 0:
-                if "app_msg_list" in resp:
-                    for item in resp["app_msg_list"]:
-                        info = self.spider.get_article_info(item)
-                        page_info.append(info)
-                    self._save_mongo(page_info)
+            if resp['base_resp']['err_msg'] == 'ok' and resp['base_resp']['ret'] == 0 and "app_msg_list" in resp:
+                for item in resp["app_msg_list"]:
+                    info = self.spider.get_article_info(item)
+                    page_info.append(info)
+                self._save_mongo(page_info)
                 print(f"{count} articles in page {begin // count + 1} have been inserted to the database.")
             begin += count
-
-    def crawl_period_posts(self, start_date, end_date):
-        pass
